be39c2a0fd31b5fb6b5456cae5bcc524df7b5909
Dan Snow <dfsnow@uchicago.edu>
2019-05-13 05:11:36 -0500

    Updated ZFS setup

--- scripts/zfs_setup_target.sh
+++ scripts/zfs_setup_target.sh
@@ -2,12 +2,13 @@
 
 # See ZFS build directions here: https://github.com/zfsonlinux/zfs/wiki/Building-ZFS
 
-# Install ZFS pool with appropriate options
-sudo zpool create -f tank -o ashift=12 -o autoreplace=on -o autoexpand=on \
-    raidz2 scsi-35000c5004117811f scsi-35000c5004117b9e3 scsi-35000cca01b4a0a34 \
-    scsi-35000cca01b4a0d54 scsi-35000cca01c401858 scsi-35000cca01c424570 \
-    raidz2 scsi-35000cca01c42a998 scsi-35000cca01c42aa58 scsi-35000cca01c42e454 \
-    scsi-35000cca01c49d45c scsi-35000cca01c49ebb0 scsi-35000cca01c49ebbc
+# Install ZFS pool with appropriate option#s
+sudo zpool create -f tank -o ashift=12 -o autoreplace=on \
+    raidz1 35000cca01c42e454 35000c5004117b9e3 35000cca01c49ebb0 35000cca01c49ebbc \
+    raidz1 35000c5004117811f 35000cca01c42a998 35000cca01b4a0d54 35000cca01b4a0a34 \
+    raidz1 35000cca01c424570 35000cca01c401858 35000cca01c42aa58 35000cca01c49d45c
+
+sudo zpool add -f tank cache nvme-INTEL_SSDPEDMW800G4_CVCQ53500085800EGN
 
 sudo zfs set atime=off tank
 sudo zfs set acltype=posixacl tank
@@ -16,8 +17,11 @@ sudo zfs set compression=lz4 tank
 sudo zfs set dedup=off tank
 sudo zfs set sync=disabled tank
 
+# Increase ARC max with the following run as root:
+#echo 60129542144 > /sys/module/zfs/parameters/zfs_arc_max
+
 # Create a zvol for connecting via iSCSI
-sudo zfs create -o compression=lz4 -o dedup=off -o volblocksize=32K -V 11T tank/primary_pool
+sudo zfs create -o compression=lz4 -o dedup=off -o volblocksize=8K -V 3T tank/psql_data
 
 # TargetCLI iscsi setup, see this link:
 # https://www.rootusers.com/how-to-configure-an-iscsi-target-and-initiator-in-linux/
@@ -25,6 +29,6 @@ sudo zfs create -o compression=lz4 -o dedup=off -o volblocksize=32K -V 11T tank/
 # Open TCP 3260 to allow all connections
 sudo apt-get install -y firewalld
 
-firewall-cmd --permanent --add-port=3260/tcp
-firewall-cmd --reload
+sudo firewall-cmd --permanent --add-port=3260/tcp
+sudo firewall-cmd --reload
 
