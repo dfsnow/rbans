9152fd7743940fb71ea9c52ad7ef525b1f7f513d
Dan Snow <dfsnow@uchicago.edu>
2019-05-20 20:35:39 -0500

    Removed pipenv dependency

--- scripts/db_load.sh
+++ scripts/db_load.sh
@@ -7,6 +7,6 @@ for file in $(ls -d "$data_dir"/*.json); do
     rm "$data_dir"/split/data_*
 
     pv "$file" | split -a 4 -l "$split_size" -d - "$data_dir"/split/data_
-    pipenv run python scripts/db_insert.py \
+    python3 scripts/db_insert.py \
         -d "$data_dir"/split -t "$max_threads" -c "$max_connections"
 done
